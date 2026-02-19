11f31924b6d5b516a6ea8c4eebb2dc9bdd190eae
tugba-koc <koctug@itu.edu.tr>
2026-02-19 22:36:44 +0300

    chore: try to use isr

--- lib/getProduct.ts
+++ lib/getProduct.ts
@@ -1,20 +1,20 @@
 interface Post {
-  id: string
-  title: string
-  content: string
+  id: string;
+  title: string;
+  content: string;
 }
 
 // Next.js will invalidate the cache when a
 // request comes in, at most once every 60 seconds.
-export const revalidate = 60
- 
+export const revalidate = 60;
+
 export async function generateStaticParams() {
-  const posts: Post[] = await fetch('https://api.vercel.app/blog').then((res) =>
-    res.json()
-  )
+  const posts: Post[] = await fetch("https://api.vercel.app/blog").then((res) =>
+    res.json(),
+  );
   return posts.map((post) => ({
     id: String(post.id),
-  }))
+  }));
 }
 
 export default async function getProduct(id: string) {
